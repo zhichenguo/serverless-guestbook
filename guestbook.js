d52abe15837b4142c340d6d8726d8766c0e8344c
Zhichen Guo <zhichenguo@gmail.com>
2022-07-20 13:17:23 -0400

    Update guestbook.js
    Add url of endpoint

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://021fe570.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
